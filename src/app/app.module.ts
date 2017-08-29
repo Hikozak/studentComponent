1af9653b3dc59fc08c640cbd43abc671089155a4
LawrBoni <31030984+Hikozak@users.noreply.github.com>
2017-08-29 16:07:10 +0800

    Bootstrap
    Applying bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
-import { StudentListComponent } from './student-list/student-list.component';
-import { StudentInputComponent } from './student-input/student-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentEntryComponent,
-    StudentListComponent,
-    StudentInputComponent
+    StudentEntryComponent
   ],
   imports: [
     BrowserModule,
